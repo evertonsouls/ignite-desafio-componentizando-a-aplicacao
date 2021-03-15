107e8a0862ef0f56da80cafabfaf49b5bea6adc8
Everton Pereira <evertonsouls@gmail.com>
2021-03-15 23:08:24 +0100

    Desafio finalizado

--- src/components/Content.tsx
+++ src/components/Content.tsx
@@ -1,3 +1,32 @@
-export function Content() {
-  // Complete aqui
+import { MovieCard } from './MovieCard';
+import '../styles/content.scss';
+
+export interface MovieProps {
+  Title: string;
+  Poster: string;
+  Ratings: Array<{
+    Source: string;
+    Value: string;
+  }>;
+  Runtime: string;
+}
+
+export interface ContentProps {
+  movies: MovieProps[]
+}
+
+export function Content(props: ContentProps) {
+  return (
+    <div className="movies-list">
+      {props.movies.map(movie => (
+        <MovieCard 
+          key={movie.Title}
+          title={movie.Title} 
+          poster={movie.Poster} 
+          runtime={movie.Runtime} 
+          rating={movie.Ratings[0].Value} 
+        />
+      ))}
+    </div>
+  )
 }
